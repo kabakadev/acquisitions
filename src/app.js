e4e0fe6b7c502a607c6e6ae20736d11ea479d531
iankabaka <iankabaka1@gmail.com>
2026-02-09 13:10:19 +0300

    cleanup of secret files

--- src/app.js
+++ src/app.js
@@ -1,8 +1,17 @@
 import express from 'express';
+import logger from '#config/logger';
+import helmet from 'helmet';
+import morgan from 'morgan';
 
 const app = express();
 
+app.use(helmet());
+app.use(express.json());
+app.use()
+
 app.get('/', (req, res) => {
+  logger.info('Hello from Acquisition!');
+
   res.status(200).send('hello from acquisitions!');
 });
 
